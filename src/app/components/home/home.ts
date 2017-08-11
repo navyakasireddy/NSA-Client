c92d7812cdc81181533aa07529fa8d17046a9a5f
navyakasireddy <navya.kasireddy@kofax.com>
2017-08-11 15:07:29 +0530

    tree structure

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -19,36 +19,16 @@ constructor(private _adminDataService: AdminDataService) {
     }  
 
   ngOnInit() {          
-        // this._adminDataService.getAdminListDetails().then((res:any) => {  
-         this.nodes =  [
-    {
-      "id": "1",
-      "name": 'root1',
-      "children": [
-        { "id": "2", "name": 'child1' },
-        { "id": "3", "name": 'child2' }
-      ]
-    },
-    {
-      "id": "4",
-      "name": 'root2',
-      "children": [
-        { "id": "5", "name": 'child2.1' },
-        {
-          "id": "6",
-          "name": 'child2.2',
-          "children": [
-            { "id": "7", "name": 'subsub' }
-          ]
-        }
-      ]
-    }
-  ];  
-        // }, (error) => {  
-             
-       // });  
+        this._adminDataService.getAdminListDetails().then((res:any) => {  
+         this.nodes =res;  }, (error) => {  
+        });  
      } 
-	 
+	 onEvent($event){
+	 	 if($event.node.children.length==0)
+		 {
+		 
+		 }
+	 }
 	 
 	/*   nodes = [
     {
